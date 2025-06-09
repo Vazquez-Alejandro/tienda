ca687b03599d7e1975c9854838019709af644423
vazquez-alejandro <vazquezale82@gmail.com>
2025-06-09 20:17:53 -0300

    primer commit

---
+++ tienda/js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        menuToggle.classList.toggle('hidden');
+    });
+});
